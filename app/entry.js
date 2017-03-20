61d08873613a4a5a97ffc3e6c60ba50ac0e889d0
caylazabel <caylazabel@gmail.com>
2017-03-20 16:55:33 -0700

    bonus working I think

--- app/entry.js
+++ app/entry.js
@@ -16,14 +16,22 @@ function CowsayController($log, $scope) {
   let cowsayCtrl = $scope.cowsayCtrl = {};
 
   cowsayCtrl.title = 'Welcome to da beach duuuude!';
+  cowsayCtrl.oldTitle ='I repeat when asked to';
+
+  cowsayCtrl.spoke = function(oldInput) {
+    $log.debug('cowsayCtrl.spoke()');
+    return cowsay.say({ text: oldInput|| 'suppppp duuuuuude', f: 'turtle' });
+  };
 
   cowsayCtrl.speak = function(input) {
     $log.debug('cowsayCtrl.speak()');
-    return cowsay.say({ text: input || 'suppppp duuuuuude', f: 'turtle' });
+    return cowsay.say({ text: input || 'suppp', f: 'turtle' });
   };
 
-  cowsayCtrl.logger = function(input) {
+
+  cowsayCtrl.logger = function(input, oldInput) {
     $log.debug('cowsayCtrl.logger()');
-    $log.log(input);
+    $log.log(input, oldInput);
+    // return cowsay.say({text: input || 'repeat me now'});
   };
 }
