c5ecf346b89e07c5e66bcac36d602d1557e3ad49
caylazabel <caylazabel@gmail.com>
2017-03-21 17:36:18 -0700

    some basics on here right now

--- app/entry.js
+++ app/entry.js
@@ -6,32 +6,60 @@ require('./scss/main.scss'); //loaders and webpack turn from static assests into
 const angular = require('angular'); //angular js//
 const cowsay = require('cowsay-browser'); //so we can interact with the cowsay browser//
 
-const cowsayApp = angular.module('cowsayApp', []); //cowsayApp is the name of the app, no other major dependencies in this structure// 
+const cowsayApp = angular.module('cowsayApp', []); //cowsayApp is the name of the app, no other major dependencies in this structure//
 
-cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+cowsayApp.controller('CowsayController', ['$log', CowsayController]);
 
-function CowsayController($log, $scope) {
+function CowsayController($log) {
   $log.debug('CowsayController');
 
-  let cowsayCtrl = $scope.cowsayCtrl = {};
+  this.title = 'Cow Creator.';
+  this.secondTitle = 'make it, view it, and undo it!';
+  this.subTitle1 = 'view it!';
+  this.subTitle2 = 'check out the cow you just made!';
+  this.history = [];
 
-  cowsayCtrl.title = 'Welcome to da beach duuuude!';
-  cowsayCtrl.oldTitle ='I repeat when asked to';
+  cowsay.list((err, cowfiles)=> {
+    this.cowfiles = cowfiles;
+    this.current = this.cowfiles[0];
+  });
 
-  cowsayCtrl.spoke = function(oldInput) {
-    $log.debug('cowsayCtrl.spoke()');
-    return cowsay.say({ text: oldInput|| 'suppppp duuuuuude', f: 'turtle' });
+
+  this.update = function(input) {
+    $log.debug('cowsayCtrl.update');
+    return cowsay.say({ text: input || 'suppppp duuuuuude', f: this.current});
   };
 
-  cowsayCtrl.speak = function(input) {
-    $log.debug('cowsayCtrl.speak()');
-    return cowsay.say({ text: input || 'suppp', f: 'turtle' });
+  this.speak = function(input) {
+    $log.debug('cowsayCtrl.speak');
+    this.spoken = this.update(input);
+    this.history.push(this.spoken);
   };
 
-
-  cowsayCtrl.logger = function(input, oldInput) {
-    $log.debug('cowsayCtrl.logger()');
-    $log.log(input, oldInput);
-    // return cowsay.say({text: input || 'repeat me now'});
+  this.undo = function() {
+    $log.debug('cowsayCtrl.undo()');
+    this.history.pop();
+    this.spoken = this.history.pop() || '';
   };
+}
+
+cowsayApp.controller('NavController',['$log', NavController]);
+
+function NavController($log) {
+  $log.debug('NavController');
+
+  this.routes = [
+    {
+      name: 'home',
+      url: '/home'
+    },
+    {
+      name: 'cow creator',
+      url: '/cowcreator'
+    },
+    {
+      name: 'account',
+      url: '/myaccount'
+    }
+  ];
 }
